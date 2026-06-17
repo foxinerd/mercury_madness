1a3a93c3f1c2270d7c8b076637dd60104210627c
foxinerd <prytelewski64@gmail.com>
2026-06-17 12:13:06 -0400

    Potent Cinnabar Block leaves behind mercury on breaking

--- src/main/java/com/foxinerd/mercury_madness/block/PotentCinnabarBlock.java
+++ src/main/java/com/foxinerd/mercury_madness/block/PotentCinnabarBlock.java
@@ -1,9 +1,19 @@
 package com.foxinerd.mercury_madness.block;
 
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
 
 public class PotentCinnabarBlock extends Block {
     public PotentCinnabarBlock(Properties properties) {
         super(properties);
+    }
+
+    @Override
+    public void destroy(final LevelAccessor level, final BlockPos pos, final BlockState state) {
+        if (!level.isClientSide()){
+            level.setBlock(pos, MMBlocks.MERCURY.defaultBlockState(), 1);
+        }
     }
 }
