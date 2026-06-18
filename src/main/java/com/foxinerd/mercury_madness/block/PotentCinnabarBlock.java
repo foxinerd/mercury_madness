534d23f9aa3ea98699ea32121381e06580afcf9e
foxinerd <prytelewski64@gmail.com>
2026-06-18 10:17:34 -0400

    Potent Cinnabar produces poison cloud when broken

--- src/main/java/com/foxinerd/mercury_madness/block/PotentCinnabarBlock.java
+++ src/main/java/com/foxinerd/mercury_madness/block/PotentCinnabarBlock.java
@@ -1,6 +1,11 @@
 package com.foxinerd.mercury_madness.block;
 
+import com.foxinerd.mercury_madness.mobeffect.MMMobEffects;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.entity.AreaEffectCloud;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
@@ -13,6 +18,12 @@ public class PotentCinnabarBlock extends Block {
     @Override
     public void destroy(final LevelAccessor level, final BlockPos pos, final BlockState state) {
         if (!level.isClientSide()){
+            AreaEffectCloud poisonCloud = new AreaEffectCloud((Level) level, pos.getX(), pos.getY(), pos.getZ());
+            poisonCloud.setRadius(2.0f);
+            poisonCloud.setDuration(100);
+            poisonCloud.setCustomParticle(ParticleTypes.NOXIOUS_GAS);
+            poisonCloud.addEffect(new MobEffectInstance(MMMobEffects.HYDRARGYRIA, 3 * 20, 0));
+            level.addFreshEntity(poisonCloud);
             level.setBlock(pos, MMBlocks.MERCURY.defaultBlockState(), 1);
         }
     }
